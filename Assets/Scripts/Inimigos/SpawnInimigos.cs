ad2c6858506d352132c2129b487a16466de995c0
Hesli Hendrik Azevedo <32403947+HesliZH@users.noreply.github.com>
2022-01-22 10:30:11 -0300

    Corrigido escala e direção de Spawn dos inimigos

--- Assets/Scripts/Inimigos/SpawnInimigos.cs
+++ Assets/Scripts/Inimigos/SpawnInimigos.cs
@@ -18,7 +18,7 @@ public class SpawnInimigos : MonoBehaviour
     private int LadoSelecionado;
 
     void Start(){
-        for(int i = 0; i >= Inimigos.Length; i++){
+        for(int i = 0; i <= Inimigos.Length; i++){
             Inimigos[i].transform.localScale = new Vector3(50, 50, 50);
         }
         //StartCoroutine(CarregaInimigos());
@@ -35,13 +35,13 @@ public class SpawnInimigos : MonoBehaviour
 
         switch(LadoSelecionado){
             case 1: 
-                Instantiate(Inimigos[Random.Range(0, Inimigos.Length)], new Vector3(Esquerda, 0.66f, Player.transform.position.z + 50), Player.transform.rotation);
+                Instantiate(Inimigos[Random.Range(0, Inimigos.Length)], new Vector3(Esquerda, 0.66f, Player.transform.position.z + 50), new Quaternion(0, 180, 0, 1));
             break;
             case 2: 
-                Instantiate(Inimigos[Random.Range(0, Inimigos.Length)], new Vector3(Meio, 0.66f, Player.transform.position.z + 50), Player.transform.rotation);
+                Instantiate(Inimigos[Random.Range(0, Inimigos.Length)], new Vector3(Meio, 0.66f, Player.transform.position.z + 50), new Quaternion(0, 180, 0, 1));
             break;
             case 3: 
-                Instantiate(Inimigos[Random.Range(0, Inimigos.Length)], new Vector3(Direita, 0.66f, Player.transform.position.z + 50), Player.transform.rotation);
+                Instantiate(Inimigos[Random.Range(0, Inimigos.Length)], new Vector3(Direita, 0.66f, Player.transform.position.z + 50), new Quaternion(0, 180, 0, 1));
             break;
         }
 
