892224df056e0f644fa54c2e6a42a14424bc7769
Hesli Hendrik Azevedo <32403947+HesliZH@users.noreply.github.com>
2022-03-01 13:23:21 -0300

    Implementado sistema de controle de carros

---
+++ Assets/Scripts/Carro/CarController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public int CarroSelecionado;
+    public GameObject[] CarrosDisponiveis;
+    public GameObject ControladorCarro;
+
+    void Start()
+    {
+        switch(CarroSelecionado){
+            case 0: 
+                ControladorCarro.transform.GetChild(0).gameObject.SetActive(true);
+            break;
+            
+            case 1: 
+                ControladorCarro.transform.GetChild(1).gameObject.SetActive(true);
+            break;
+
+        }
+    }
+}
